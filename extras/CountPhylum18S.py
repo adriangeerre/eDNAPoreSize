902b9aa635fb8459820b28e8b412ca303fcfc147
Adrian Geerre <adriangomezrepolles@gmail.com>
2025-10-26 08:47:28 +0100

    clean comment

--- extras/CountPhylum18S.py
+++ extras/CountPhylum18S.py
@@ -12,10 +12,6 @@ parser.add_argument('-b', '--blast', dest='blast', action='store', help='18S rib
 # parser.add_argument('-p', '--pident', dest='pident', action='store', help='Percentage of identity to filter 18S blast results.', required=True, type=int)
 parser.add_argument('-o', '--outfolder', dest='outfolder', action='store', help='Output folder to save results.', required=True)
 
-### -i: 06-mergeLCA/A0_2.lca.tsv
-### -a: 10-Estimate18S/90/A0_2
-### -b: 11-Blast_r18S/blast_results/A0_2_r18S.tsv
-
 ## Functions
 
 # List files
